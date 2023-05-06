549f3096d324031fe7cc82c31b2cc3e513040c72
hadjersh97 <jijouinfo@gmail.com>
2023-05-06 18:49:48 +0100

    some old modification comitted

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -2,10 +2,30 @@ import { useState, useEffect } from 'react'
 import './About.scss'
 
 const About = () => {
-
+  
+  const [animatedClass, setAnimatedClass] = useState(' animation');
+  function resolveAfter60Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 9000);
+    });
+  }
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter60Seconds();
+    setAnimatedClass('');
+    console.log(result);
+    // expected output: "resolved"
+  }
+  useEffect(() => {
+    console.log('class is before',animatedClass)
+    //asyncCall();
+    console.log('class is',animatedClass)
+  })
   return (
     <div>
-        <div className='about-animated' >
+        <div className={`about-animated`} >
               
                   <h2>About me</h2>
             
